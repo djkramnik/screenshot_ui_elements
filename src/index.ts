dc416b8f48a8c2ebc5f076764c3daec5a0b30150
David Gurr <eggsandtobasco@gmail.com>
2021-03-11 13:54:32 -0500

    the script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,39 @@
 import { Page } from 'puppeteer'
 import { PUPPETEER_LAUNCH_OPTIONS } from './config';
-import { getPageRef } from './util'
+import { getPageRef, screenshotBoundingRect } from './util'
 
-let page: Page | null = null
+const defaultArgs = {
+  url: 'www.youtube.com',
+  selector: 'button',
+}
+
+const scriptArgs = {
+  url: process.argv[2] ?? defaultArgs.url,
+  selector: process.argv[3] ?? defaultArgs.selector,
+}
 
 ;(async () => {
 
-  page = await getPageRef(PUPPETEER_LAUNCH_OPTIONS)
+  const page = await getPageRef(PUPPETEER_LAUNCH_OPTIONS)
+
+  await page.goto(scriptArgs.url)
+  const boundingRectsForScreenshots = await page.evaluate(() => {
+    const boundingRectsInViewport = Array.from(
+      document.querySelectorAll(scriptArgs.selector)
+    ).reduce((boxes, elem) => {
+      const rect = elem.getBoundingClientRect()
+      const {top, left, bottom, right} = rect
+      const isInViewport = (
+        top >= 0 &&
+        left >= 0 &&
+        bottom <= window.innerHeight &&
+        right <= window.innerWidth
+      )
+      return isInViewport ? boxes.concat(rect) : boxes
+    }, [] as DOMRect[])
+    return boundingRectsInViewport
+  })
+  await Promise.all(boundingRectsForScreenshots.map((boundingRect, index) => {
+    return screenshotBoundingRect({page, boundingRect, path: `screen_${index}.png`, padding: 20})
+  }))
 })()
